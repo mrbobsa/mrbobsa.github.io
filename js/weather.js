664e2c3ac6de052bd33e09c3683fdf8ab7dce260
chntuni <ds3dxf@hanmail.net>
2022-01-24 00:20:40 +0900

    Add files via upload

---
+++ js/weather.js
@@ -0,0 +1,20 @@
+const API_KEY = "029e8611e99749e14479d9991cdc6713";
+
+function onGeoOk(position) {
+  const lat = position.coords.latitude;
+  const lon = position.coords.longitude;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      const weather = document.querySelector("#weather span:first-child");
+      const city = document.querySelector("#weather span:last-child");
+      weather.innerText = `${data.weather[0].main} / ${data.main.temp}˚C`;
+      city.innerText = data.name;
+    });
+}
+function onGeoError() {
+  alert("Can't find you. No weather for you.");
+}
+
+navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
